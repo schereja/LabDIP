d0bac78a6ec8cf5173142c8e0df77d727c4831f3
schereja <schereja@10.30.17.193>
2013-09-14 19:58:08 -0500

    Redid more of lab2, started to work on lab 1

--- src/dip/lab2/student/solution1/Startup.java
+++ src/dip/lab2/student/solution1/Startup.java
@@ -9,6 +9,10 @@ package dip.lab2.student.solution1;
  * @author schereja
  */
 public class Startup {
-   
+   public static void main(String[] args){
+       TipCalculator foodTip = new FoodServiceTipCalculator(TipCalculator.ServiceQuality.GOOD, 50);
+       TipCalculator baggageTip = new BaggageServiceTipCalculator(TipCalculator.ServiceQuality.GOOD, 5);
+      
+   }
     
 }
